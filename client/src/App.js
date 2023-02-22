fa94b8ec31e994d21abbca9506d21fe7a082e06e
Bipul Swain <bipulswain365@gmail.com>
2023-02-22 22:13:27 +0530

    adding assets, components-signin,signup,nav,

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import { Collapse } from "flowbite";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='relative h-screen bg-gradient-to-tl from-black to-[#330c46]'>
+      <Router>
+        <Navbar />
+        <div >
+          <Routes>
+            <Route exact path='/' element={<Signin />} />
+            <Route exact path='/Signup' element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
